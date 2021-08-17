283c8a38b9902445e240cdecd3b5486bfcda598c
hamlinkrewson <87337864+hamlinkrewson@users.noreply.github.com>
2021-08-17 12:44:07 -0500

    Update getWMPanicCount.sh

--- getWMPanicCount.sh
+++ getWMPanicCount.sh
@@ -1,22 +1,50 @@
-#!/bin/bash
-# Watchman Monitoring keeps track of Kernel Panics
-#	Check for the quanity of currently tracked panic logs
+#
+# Copyright (c) 2021 Hamlin Krewson
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# 	limitations under the License.
+#
+################################# DESCRIPTION #################################
+# 
+#	This script looks for Kernel Panics on macOS 10.12 or later.
+#
+#################################### TO DO ####################################
+#
+#
+################################## VARIABLES ##################################
 
-# Get the kernel panic count
-panicCount=$(/usr/libexec/PlistBuddy -c 'print :Last_Panic_Count' /Library/MonitoringClient/PluginSupport/check_panic_count_settings.plist)
 
-
-# If there are panics recorded, print the count and the recorded dates
-if [[ $panicCount == *"Does Not Exist"* ]]; then 
-	echo "<result>No Panics</result>"
-else 
-	echo "Panic Count: "$panicCount
-	i=0
-    panicResults=()
-    while [ $i -lt $panicCount ]; do
-    		panicResults+=("$(echo "Panic Date: " $(/usr/libexec/PlistBuddy -c "print :Panic_List:$i" /Library/MonitoringClient/PluginSupport/check_panic_count_settings.plist | xargs date -r))")
-        	echo ${panicResults[$i]}
-		i=$(($i + 1))     
-	done
+################################## FUNCTIONS ##################################
+function result(){
+	# For Jamf Pro the following should be uncommented
+	echo "<result>$1</result>"
 	
-fi
+	#for Mosyle, the following should be uncommented
+	#printf "$1"
+}
+
+
+# Do things. Send output to the result() function. 
+#	result "my intended extension attribute info"
+################################### Examples ##################################
+#IFSOLD=$IFS
+#IFS=$'\n'
+#hasAppSaved=($(mdfind "AppName.app"))
+#for i in $hasAppSaved; do
+#	if [[ "$i" =~ ".app" ]]; then
+#		result "$i"
+#	fi
+#done
+
+
+panicList=($(mdfind 'kMDItemFSName == *.panic'))
+result ${#panicList[@]}
